1356f82722e475376fbcd5a4e4ca4e3f3ee42df1
Vansh506 <vansh@vigyani.ai>
2025-08-10 19:08:09 +0530

    feat:intial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
+import { ChatModule } from './chat/chat.module';
+import { CityModule } from './city/city.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductModule, ChatModule, CityModule, WarehouseModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
