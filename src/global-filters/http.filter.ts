fe372a9e91d2f7340eb9cbb9b7dbc437b85f70fc
Van5sh <vansh05dhir@gmail.com>
2025-08-15 23:50:00 +0530

    feat:filter exception

--- src/global-filters/http.filter.ts
+++ src/global-filters/http.filter.ts
@@ -11,13 +11,17 @@ export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    const message = exception.message || 'Internal server error';
+    const code =
+      exception instanceof HttpException ? exception.getStatus() : 500;
 
     response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
+      status: 'errr',
+      statusCode: code,
+      message,
     });
   }
 }
