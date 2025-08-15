3ec31cbc2bef874f55e95cb5204ce6506cbab45b
Van5sh <vansh05dhir@gmail.com>
2025-08-15 21:19:28 +0530

    fix:user dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/database.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+}
