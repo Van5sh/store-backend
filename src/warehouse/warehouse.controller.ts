e27626781c0b8f58afd323b122cb331794faa9bb
Van5sh <vansh05dhir@gmail.com>
2025-08-25 14:38:21 +0530

    fix:Create warehouse contoller

--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { WarehouseService } from './warehouse.service';
-
+import { CreateWarehouseDto } from './dto/create-warehouse.dto';
 @Controller('warehouse')
 export class WarehouseController {
   constructor(private readonly warehouseService: WarehouseService) {}
   @Get()
   async findAll() {
     return this.warehouseService.allWarehouses();
+  }
+  @Post()
+  async createWarehouse(@Body() data: CreateWarehouseDto) {
+    return this.warehouseService.createWarehouse(data);
   }
 }
